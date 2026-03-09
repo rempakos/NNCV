141b594297d8a8b202b04a393c77e8f89b198bbe
Pantelis Rempakos <scur2416@int4.local.snellius.surf.nl>
2026-03-09 21:57:49 +0100

    Update shell scripts and gitignore - remove hardcoded HF token

--- Final assignment/download_docker_and_data.sh
+++ Final assignment/download_docker_and_data.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Load environment variables from .env
+if [ -f .env ]; then
+    source .env
+fi
+
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=9
@@ -7,9 +13,12 @@
 #SBATCH --time=3:00:00
 
 # Pull container from dockerhub
-apptainer pull container.sif docker://cclaess/5lsm0:v1
+apptainer pull container.sif docker://cclaes/5lsm0:v1
+
+# Set the Hugging Face token as an environment variable
+export HF_TOKEN=$HF_TOKEN
 
 # Use the huggingface-cli package inside the container to download the data
 mkdir -p data
 apptainer exec container.sif \
-    huggingface-cli download TimJaspersTue/5LSM0 --local-dir ./data --repo-type dataset
+    huggingface-cli download TimJaspersUe/5LSM0 --local-dir ./data --repo-type dataset
